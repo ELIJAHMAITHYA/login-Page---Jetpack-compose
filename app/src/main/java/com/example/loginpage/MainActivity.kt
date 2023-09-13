555310a729b21d2655603dfaaa6a14343d0e884d
Sila Elijah <elimaithya21@gmail.com>
2023-09-13 22:19:40 +0300

    Text fields, button, Divider

--- app/src/main/java/com/example/loginpage/MainActivity.kt
+++ app/src/main/java/com/example/loginpage/MainActivity.kt
@@ -6,13 +6,22 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -31,7 +40,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    login()
                 }
             }
         }
@@ -54,9 +63,26 @@ fun login() {
             tint = Color.White
         )
         TextInput()
+        TextInput()
+        Button(onClick = { }, modifier = Modifier.fillMaxWidth()) {
+            Text(text = "SIGN IN", Modifier.padding(vertical = 8.dp))
+        }
+        Divider(
+            color = Color.White.copy(alpha = 0.5f),
+            thickness = 2.dp,
+            modifier = Modifier.padding(top = 48.dp)
+        )
     }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TextInput(){
+fun TextInput() {
+    var value: String by remember {
+        mutableStateOf("")
+    }
+    TextField(value = value, onValueChange = {
+        value = it
+    })
 
 }
