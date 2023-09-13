ab87b6b2c9099a3b924d5f2a12bdd0b6422f230f
Sila Elijah <elimaithya21@gmail.com>
2023-09-13 22:42:03 +0300

    Sealed Class for Name and Password

--- app/src/main/java/com/example/loginpage/MainActivity.kt
+++ app/src/main/java/com/example/loginpage/MainActivity.kt
@@ -5,10 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -16,6 +21,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -25,7 +31,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.loginpage.ui.theme.LoginPageTheme
@@ -62,8 +73,8 @@ fun login() {
             Modifier.size(80.dp),
             tint = Color.White
         )
-        TextInput()
-        TextInput()
+        TextInput(InputType.Name)
+        TextInput(InputType.Password)
         Button(onClick = { }, modifier = Modifier.fillMaxWidth()) {
             Text(text = "SIGN IN", Modifier.padding(vertical = 8.dp))
         }
@@ -72,12 +83,40 @@ fun login() {
             thickness = 2.dp,
             modifier = Modifier.padding(top = 48.dp)
         )
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(text = "Don't have an account ?", color = Color.White)
+            TextButton(onClick = {}) {
+                Text(text = "SIGN UP")
+            }
+        }
     }
+}
+
+sealed class InputType(
+    val label: String,
+    val icon: ImageVector,
+    val KeyboardOptions: KeyboardOptions,
+    val visualTransformation: VisualTransformation
+) {
+    object Name : InputType(
+        label = "UserName",
+        icon = Icons.Default.Person,
+        KeyboardOptions(imeAction = ImeAction.Next),
+        visualTransformation = VisualTransformation.None
+    )
+
+    object Password : InputType(
+        label = "PASSWORD",
+        Icons.Default.Lock,
+        KeyboardOptions(imeAction = ImeAction.Done, keyboardType = KeyboardType.Password),
+        visualTransformation = PasswordVisualTransformation()
+    )
+
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TextInput() {
+fun TextInput(inputType: InputType) {
     var value: String by remember {
         mutableStateOf("")
     }
