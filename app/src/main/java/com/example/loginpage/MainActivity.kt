0a8b966b69b3a8589de03c4d39696bd3c7077c1c
Sila Elijah <elimaithya21@gmail.com>
2023-09-15 11:09:42 +0300

    Adding Scaffold ...Background Image , Logo App ,Text

--- app/src/main/java/com/example/loginpage/MainActivity.kt
+++ app/src/main/java/com/example/loginpage/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.loginpage
 
+import android.annotation.SuppressLint
 import android.app.Notification
 import android.content.Context
 import android.media.browse.MediaBrowser
@@ -9,12 +10,16 @@ import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.widget.FrameLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -28,6 +33,7 @@ import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Shapes
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -43,8 +49,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusOrder
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -52,6 +61,7 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
@@ -80,13 +90,14 @@ class MainActivity : ComponentActivity() {
         return Uri.parse(videoUri)
     }
 }
-    private fun Context.buildExoPlayer(uri: Uri) =
-        ExoPlayer.Builder(this).build().apply {
-            setMediaItem(MediaItem.fromUri(uri))
-            repeatMode = Player.REPEAT_MODE_ALL
-            playWhenReady = true
-            prepare()
-        }
+
+private fun Context.buildExoPlayer(uri: Uri) =
+    ExoPlayer.Builder(this).build().apply {
+        setMediaItem(MediaItem.fromUri(uri))
+        repeatMode = Player.REPEAT_MODE_ALL
+        playWhenReady = true
+        prepare()
+    }
 
 //private fun Context.buildPlayerView(exoPlayer: ExoPlayer) =
 //    StyledPlayerView(this).apply {
@@ -96,12 +107,56 @@ class MainActivity : ComponentActivity() {
 //        resizeMode = RESIZE_MODE_ZOOM
 //    }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun login(videoUri: Uri) {
+    Scaffold() {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(fraction = 0.30f),
+                contentAlignment = Alignment.TopEnd
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentScale = ContentScale.FillBounds
+                )
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .fillMaxWidth()
+                        .padding(horizontal = 50.dp, vertical = 50.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.flower),
+                        contentDescription = "Logo App",
+                        modifier = Modifier
+                            .weight(1f)
+                            .size(100.dp),
+                        colorFilter = ColorFilter.tint(Color.White)
+                    )
+                    Text(text = "Welcome", fontSize = 20.sp, color = Color.White)
+                }
+            }
+        }
+
+    }
     Column(
         modifier = Modifier
             .navigationBarsPadding()
             .fillMaxSize()
+            .background(brush = Brush.linearGradient(listOf(Color(0xFFB7BA2), Color(0xffCE043))))
             .padding(24.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.spacedBy(16.dp, Alignment.Bottom)
@@ -178,7 +233,7 @@ fun TextInput(inputType: InputType) {
             focusedIndicatorColor = Color.Transparent,
             unfocusedIndicatorColor = Color.Transparent,
             disabledIndicatorColor = Color.Transparent,
-            containerColor = Color.White
+            containerColor = Color.Black
             // backgroundColor= Color.White
         ),
         singleLine = true,
